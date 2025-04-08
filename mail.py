db7ff7c50eb5eed41371e73eb5fd084d6d8ede93
DAM <43420467+DAMcraft@users.noreply.github.com>
2025-04-08 16:19:12 +0200

    use email.message

--- mail.py
+++ mail.py
@@ -1,58 +1,54 @@
 import random
 import smtplib
-import textwrap
+from email.message import EmailMessage
 import const
 
 
 def send_welcome_email(email, username):
     subject = "welcome to freearpa.damcraft.de"
-    body = textwrap.dedent(f"""\
-        Subject: {subject}
-        From: {const.EMAIL_FROM}
-        To: {email}
-        Message-ID: <{random.randint(1, 1000000)}@damcraft.de> 
+    body = f"""\
+hey, {username}!
 
-        hey, {username}!
+your account was successfully created.
+if you have any questions, feel free to reach out!
 
-        your account was successfully created.
-        if you have any questions, feel free to reach out!
-
-        best regards,
-        dami
-    """)
-
-    return send_email(email, body)
+best regards,
+dami
+"""
+    return send_email(email, subject, body)
 
 
 def send_forgot_password_email(email, username, reset_token):
     subject = "freearpa - password reset request"
-    body = textwrap.dedent(f"""\
-        Subject: {subject}
-        From: {const.EMAIL_FROM}
-        To: {email}
-        Message-ID: <{random.randint(1, 1000000)}@damcraft.de> 
+    body = f"""\
+hey, {username}!
 
-        hey, {username}!
+you requested a password reset.
+open the link below to reset your password:
+https://freearpa.damcraft.de/reset-password/{reset_token}
 
-        you requested a password reset.
-        open the link below to reset your password:
-        https://freearpa.damcraft.de/reset-password/{reset_token}
+if you didn't request this, please ignore this email.
 
-        if you didn't request this, please ignore this email.
-
-        best regards,
-        dami
-    """)
-
-    return send_email(email, body)
+best regards,
+dami
+"""
+    return send_email(email, subject, body)
 
 
-def send_email(recipient, body):
+def send_email(recipient, subject, body):
     try:
+        msg = EmailMessage()
+        msg["From"] = const.EMAIL_FROM
+        msg["To"] = recipient
+        msg["Subject"] = subject
+        msg["Message-ID"] = f"<{random.randint(1, 1_000_000)}@damcraft.de>"
+        msg.set_content(body)
+
         with smtplib.SMTP(const.SMTP_SERVER, const.SMTP_PORT) as server:
             server.starttls()
             server.login(const.SMTP_USERNAME, const.SMTP_PASSWORD)
-            server.sendmail(const.EMAIL_FROM, recipient, body)
+            server.send_message(msg)
+
         return True
     except Exception as e:
         print(f"Failed to send email: {e}")
